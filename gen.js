ffcc966b9c59ca692ad463670311ab63bbcd26cd
Divyaprakash-12m <you@example.com>
2023-03-03 21:18:00 +0530

    committed

--- gen.js
+++
@@ -1,9 +0,0 @@
-function gender(){
-    var gen=document.getElementById("g").value;
-    if(gen=='m' || gen=='M')
-    document.write("The person is a male");
-    else if(gen=='f' || gen=='F')
-    document.write("the person is a female");
-    else
-    document.write("invalid input");
-}
